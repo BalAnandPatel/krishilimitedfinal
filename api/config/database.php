0bde08af60193395626dbb07f703853f387bd686
Manu Computer <giplanand@gmail.com>
2023-04-18 11:33:00 +0530

    updats

--- api/config/database.php
+++ api/config/database.php
@@ -3,10 +3,10 @@ date_default_timezone_set("Asia/Kolkata");
 class Database{
   
     // specify your own database credentials
-    private $host = "ssegr.org.in";
-    private $db_name = "glintqnj_recruitment";
-    private $username = "glintqnj_recruitment";
-    private $password = "12qw!@QW9984";
+    private $host = "localhost";
+    private $db_name = "krishilimited";
+    private $username = "root";
+    private $password = "root";
     public $conn;
   
     // get the database connection
